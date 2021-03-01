73dec4331bf98a796787cee56a346ae277567cc8
Daniel-KI <dankorshov@gmail.com>
2021-03-01 14:03:22 +0300

    feat: add hints toggle

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-const KEYS = ['KeyA','KeyS','KeyD','KeyF','KeyG','KeyH','KeyJ','KeyW','KeyE','KeyT','KeyY','KeyU'];
+const KEYS = ['KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyW', 'KeyE', 'KeyT', 'KeyY', 'KeyU'];
 const PATH_TO_SOUNDS = './assets/sounds/';
 const pianoKeys = document.querySelectorAll('.piano-key');
 
 const fullScreenBtn = document.querySelector('.full-screen');
 const soundTypeBtn = document.querySelector('.sound-type');
+const notesHintsBtn = document.querySelector('.note-hints');
+const keyboardHintsBtn = document.querySelector('.keyboard-hints');
 
 const soundBanks = new Map([
     ['Classical', 'classical/'],
@@ -15,50 +17,50 @@ let mouseButtonDown = false;
 
 document.addEventListener('mousedown', () => {
     mouseButtonDown = true;
-})
+});
 document.addEventListener('mouseup', () => {
-    mouseButtonDown = false
-})
+    mouseButtonDown = false;
+});
 
 pianoKeys.forEach(key => {
     key.addEventListener('mousedown', () => {
         key.classList.add('active-key');
         playNote(key);
-    })
+    });
     key.addEventListener('mouseup', () => {
         key.classList.remove('active-key');
-    })
+    });
     key.addEventListener('mouseenter', () => {
-        if(mouseButtonDown === true){
+        if (mouseButtonDown === true) {
             key.classList.add('active-key');
             playNote(key);
         }
-    })
+    });
     key.addEventListener('mouseleave', () => {
         key.classList.remove('active-key');
-    })
+    });
 });
 
 document.addEventListener('keydown', e => {
     if (e.repeat) return;
     const key = e.code;
     const keyIndex = KEYS.indexOf(key);
-    if(keyIndex > -1){
+    if (keyIndex > -1) {
         playNote(pianoKeys[keyIndex]);
         pianoKeys[keyIndex].classList.add('active-key');
     }
 });
 
-document.addEventListener('keyup', e =>{
+document.addEventListener('keyup', e => {
     if (e.repeat) return;
     const key = e.code;
     const keyIndex = KEYS.indexOf(key);
-    if(keyIndex > -1){
+    if (keyIndex > -1) {
         pianoKeys[keyIndex].classList.remove('active-key');
     }
 });
 
-function playNote(key){
+function playNote(key) {
     const path = `${PATH_TO_SOUNDS}${soundBanks.get(selectedSoundType)}${key.dataset.note}.wav`;
     const noteSound = new Audio(path);
     noteSound.play();
@@ -68,9 +70,9 @@ fullScreenBtn.addEventListener('click', () => {
     if (!document.fullscreenElement) {
         document.documentElement.requestFullscreen();
     } else {
-      if (document.exitFullscreen) {
-        document.exitFullscreen();
-      }
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
     }
 });
 
@@ -78,15 +80,38 @@ document.addEventListener('fullscreenchange', () => {
     document.querySelector('.expand-icon').classList.toggle('hidden-icon');
     document.querySelector('.compress-icon').classList.toggle('hidden-icon');
     fullScreenBtn.classList.toggle('btn-selected');
-})
+});
 
 soundTypeBtn.addEventListener('click', () => {
     soundTypeBtn.classList.toggle('btn-selected');
-    if(selectedSoundType === 'Classical'){
+    if (selectedSoundType === 'Classical') {
         selectedSoundType = 'Electronic';
     }
-    else{
+    else {
         selectedSoundType = 'Classical';
     }
     document.querySelector('.sound-type_name').textContent = selectedSoundType;
-})
+});
+
+
+notesHintsBtn.addEventListener('click', () => {
+    if (keyboardHintsBtn.classList.contains('btn-selected-hint')) {
+        keyboardHintsBtn.classList.remove('btn-selected-hint');
+        notesHintsBtn.classList.toggle('btn-selected-hint');
+        pianoKeys.forEach(key => {
+            key.classList.toggle('note-hint');
+            key.classList.toggle('key-hint');
+        })
+    }
+});
+keyboardHintsBtn.addEventListener('click', () => {
+    if (notesHintsBtn.classList.contains('btn-selected-hint')) {
+        notesHintsBtn.classList.remove('btn-selected-hint');
+        keyboardHintsBtn.classList.toggle('btn-selected-hint');
+        pianoKeys.forEach(key => {
+            key.classList.toggle('key-hint');
+            key.classList.toggle('note-hint');
+        })
+    }
+});
+
